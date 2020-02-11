34ca21e3dfe19979fe14aab4b6263e3cc3739d0d
Pedro Martins <pedro@nancode.com.br>
2020-02-11 18:05:03 -0300

    Escopo inicial do projeto

---
+++ TensorFlowWithCAndCSharp/TensorFlowWithCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TensorFlowWithCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
